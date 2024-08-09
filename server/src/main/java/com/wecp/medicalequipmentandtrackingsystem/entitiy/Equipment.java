b30c693428ace6ce140fb281720b1cced4e786a8
username <wecp-dummy@gmail.com>
2024-08-09 13:06:32 +0000

    wecp initial commit

---
+++ server/src/main/java/com/wecp/medicalequipmentandtrackingsystem/entitiy/Equipment.java
@@ -0,0 +1,10 @@
+package com.wecp.medicalequipmentandtrackingsystem.entitiy;
+
+
+import javax.persistence.*;
+
+@Table(name = "equipments") // do not change table name
+public class Equipment {
+
+
+}
